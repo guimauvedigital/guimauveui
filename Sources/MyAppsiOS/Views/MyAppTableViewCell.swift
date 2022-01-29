501ee2962656e9a787e42d3f693fd3a66c914aff
Nathan Fallet <contact@nathanfallet.me>
2022-01-29 17:26:39 +0100

    Fixing class name issues

--- Sources/MyAppsiOS/Views/MyAppTableViewCell.swift
+++ Sources/MyAppsiOS/Views/MyAppTableViewCell.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-public class AppTableViewCell: UITableViewCell {
+public class MyAppTableViewCell: UITableViewCell {
     
     let icon = UIImageView()
     let name = UILabel()
@@ -38,25 +38,25 @@ public class AppTableViewCell: UITableViewCell {
         
         name.translatesAutoresizingMaskIntoConstraints = false
         name.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor).isActive = true
-        name.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 8).isActive = true
+        name.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 12).isActive = true
         name.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
         
         desc.translatesAutoresizingMaskIntoConstraints = false
         desc.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 4).isActive = true
-        desc.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 8).isActive = true
+        desc.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 12).isActive = true
         desc.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
         desc.bottomAnchor.constraint(lessThanOrEqualTo: contentView.layoutMarginsGuide.bottomAnchor).isActive = true
         desc.font = .systemFont(ofSize: 15)
+        desc.textColor = .systemGray
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func with(app: MyApp) -> AppTableViewCell {
+    public func with(app: MyApp) -> MyAppTableViewCell {
         self.name.text = app.name
         self.desc.text = NSLocalizedString(app.description, bundle: .module, comment: app.description)
-        self.desc.textColor = .systemGray
         self.icon.image = UIImage(named: app.icon, in: .module, compatibleWith: nil)
         self.url = app.url
         
