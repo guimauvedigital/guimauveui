9564b05509ba58dcaadc7fe4a08bc924e594a591
Nathan Fallet <contact@nathanfallet.me>
2022-02-10 19:16:53 +0100

    Updating Converty

--- Sources/MyAppsiOS/Models/MyApp.swift
+++ Sources/MyAppsiOS/Models/MyApp.swift
@@ -36,10 +36,10 @@ public struct MyApp: Identifiable {
             url: "https://apps.apple.com/app/latex-cards/id1598813588"
         ),
         MyApp(
-            name: "BaseConverter: All in one",
-            description: "baseconverter",
-            icon: "BaseConverter",
-            url: "https://apps.apple.com/app/baseconverter-all-in-one/id1446344899"
+            name: "Converty: Number conversion",
+            description: "converty",
+            icon: "Converty",
+            url: "https://apps.apple.com/app/converty-number-conversion/id1609456234"
         ),
         MyApp(
             name: "Delta: Algorithms",
