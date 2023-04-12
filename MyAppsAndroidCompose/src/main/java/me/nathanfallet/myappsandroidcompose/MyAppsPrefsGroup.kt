2059e70d2c22a231087a26980a507b82fe447a0f
NathanFallet <contact@nathanfallet.me>
2023-04-13 00:40:03 +0200

    Adding Compose module

---
+++ MyAppsAndroidCompose/src/main/java/me/nathanfallet/myappsandroidcompose/MyAppsPrefsGroup.kt
@@ -0,0 +1,21 @@
+package me.nathanfallet.myappsandroidcompose
+
+import androidx.compose.ui.res.stringResource
+import com.jamal.composeprefs.ui.GroupHeader
+import com.jamal.composeprefs.ui.PrefsScope
+import me.nathanfallet.myappsandroid.R
+import me.nathanfallet.myappsandroid.models.MyApp
+
+fun PrefsScope.myApps() {
+    prefsGroup({
+        GroupHeader(
+            title = stringResource(id = R.string.header)
+        )
+    }) {
+        MyApp.values().forEach { app ->
+            prefsItem {
+                MyAppPref(app)
+            }
+        }
+    }
+}
--- app/src/main/java/me/nathanfallet/myappsandroidsample/SectionsPagerAdapter.kt
+++ app/src/main/java/me/nathanfallet/myappsandroidsample/SectionsPagerAdapter.kt
@@ -7,7 +7,8 @@ import androidx.fragment.app.FragmentPagerAdapter
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -20,7 +21,9 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return if (position == 0) SettingsFragment() else RecyclerViewFragment()
+        return if (position == 0) SettingsFragment()
+        else if (position == 1) RecyclerViewFragment()
+        else ComposeFragment()
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -28,7 +31,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return 3
     }
 }
